09c29b40ab41b4f115b15ee5bc9d63f20429f24d
saadazghour <saad.azghour@gmail.com>
2023-07-31 11:17:08 +0100

    Add Icons component

--- src/app/components/SideBarInActive.tsx
+++ src/app/components/SideBarInActive.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const SideBarIcon = () => {
+const SideBarInActive = () => {
   return (
     <svg
       width="20"
@@ -10,36 +10,36 @@ const SideBarIcon = () => {
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
-        d="M10.0001 18.3333C14.6001 18.3333 18.3334 14.6 18.3334 10C18.3334 5.4 14.6001 1.66666 10.0001 1.66666C5.40008 1.66666 1.66675 5.4 1.66675 10C1.66675 14.6 5.40008 18.3333 10.0001 18.3333Z"
-        stroke="#18181B"
+        d="M10.0001 18.3333C14.6001 18.3333 18.3334 14.6 18.3334 10C18.3334 5.4 14.6001 1.66667 10.0001 1.66667C5.40008 1.66667 1.66675 5.4 1.66675 10C1.66675 14.6 5.40008 18.3333 10.0001 18.3333Z"
+        stroke="#A1A1AA"
         strokeWidth="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
-        d="M10.0001 13.3333C11.8417 13.3333 13.3334 11.8417 13.3334 10C13.3334 8.15833 11.8417 6.66666 10.0001 6.66666C8.15841 6.66666 6.66675 8.15833 6.66675 10C6.66675 11.8417 8.15841 13.3333 10.0001 13.3333Z"
-        stroke="#18181B"
+        d="M10.0001 13.3333C11.8417 13.3333 13.3334 11.8417 13.3334 10C13.3334 8.15833 11.8417 6.66667 10.0001 6.66667C8.15841 6.66667 6.66675 8.15833 6.66675 10C6.66675 11.8417 8.15841 13.3333 10.0001 13.3333Z"
+        stroke="#A1A1AA"
         strokeWidth="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
         d="M17.6417 6.66667C15.125 6.11667 12.5167 6.11667 10 6.66667"
-        stroke="#18181B"
+        stroke="#A1A1AA"
         strokeWidth="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
-        d="M3.29175 5.05L3.30841 5.1C4.15008 7.50834 5.44175 9.74167 7.11675 11.6667"
-        stroke="#18181B"
+        d="M3.29175 5.05L3.30841 5.1C4.15008 7.50833 5.44175 9.74167 7.11675 11.6667"
+        stroke="#A1A1AA"
         strokeWidth="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
         d="M9.06665 18.2833C10.7833 16.3917 12.075 14.1583 12.8583 11.7333L12.8833 11.6667"
-        stroke="#18181B"
+        stroke="#A1A1AA"
         strokeWidth="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"
@@ -48,4 +48,4 @@ const SideBarIcon = () => {
   );
 };
 
-export default SideBarIcon;
+export default SideBarInActive;
